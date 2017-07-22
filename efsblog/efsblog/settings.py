680d4540b6c42983a0eceaf443118909c633e53e
Shivani Singh <ssinghparihar@unomaha.edu>
2017-07-22 15:14:45 -0500

    additional file changes for heroku

--- efsblog/efsblog/settings.py
+++ efsblog/efsblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')=jxlohre_*x8ed@#w(lkvutiv&%h15@3-@pl*4_us!jde$456'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = []
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
 # Application definition
@@ -79,10 +87,10 @@ WSGI_APPLICATION = 'efsblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'efs_database',
-        'USER': 'efsuser',
-        'PASSWORD' : 'efspassword',
-        'HOST' : '127.0.0.1',
+        'NAME': 'd319ub2b2mvco7',
+        'USER': 'erjqjqureguhdl',
+        'PASSWORD' : '114c4107c140508b004cb839e21ac5f559effc56f3e8b34d82a4c3cdd14cd214',
+        'HOST' : 'ec2-23-23-86-179.compute-1.amazonaws.com',
         'PORT' : '5432',
     }
 }
@@ -124,4 +132,30 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+
+STATICFILES_DIRS = (
+
+os.path.join(PROJECT_ROOT, 'static'),
+
+)
+
+
+# Update database configuration with $DATABASE_URL.
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
+
+
+# Simplified static file serving.
+
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
